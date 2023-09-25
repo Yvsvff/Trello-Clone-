ab68ad6980008b58699944792ca7e43669c382b2
Yusuff Atanda <Yvsvfff@gmail.com>
2023-09-25 11:26:56 -0400

    update

--- trello-clone/app/page.tsx
+++ trello-clone/app/page.tsx
@@ -1,7 +1,14 @@
+import Board from "@components/Board";
+import Header from "@components/Header";
 
 
 export default function Home() {
   return (<main>
+    {/*Header */}
+    <Header />
+
+    {/*Board */}
+    <Board />
     <h1>
       Trello Clone
     </h1>
