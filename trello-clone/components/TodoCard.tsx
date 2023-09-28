8e832a2c403e940af257fac847f02d4ac9cc25f5
Yusuff Atanda <Yvsvfff@gmail.com>
2023-09-27 20:11:41 -0400

    update

--- trello-clone/components/TodoCard.tsx
+++ trello-clone/components/TodoCard.tsx
@@ -1,5 +1,6 @@
 'use client'
 import { XCircleIcon } from "@heroicons/react/20/solid";
+import { useBoardStore } from "@store/BoardStore";
 import { DraggableProvidedDragHandleProps, DraggableProvidedDraggableProps } from "react-beautiful-dnd";
 
 
@@ -15,6 +16,9 @@ export default function TodoCard({
     todo, index, id,
     innerRef, draggableProps,dragHandleProps,
 }: Props) {
+
+const deleteTask = useBoardStore((state) => state.deleteTask);
+
   return (
     <div
     className="bg-white rounded-md space-y-2 drop-shadow-md"
@@ -24,7 +28,7 @@ export default function TodoCard({
     >
         <div className="flex justify-between items-center p-5">
           <p>{todo.title}</p>
-          <button className="text-red-500 hover:text-red-600">
+          <button onClick={() => deleteTask(index, todo, id)} className="text-red-500 hover:text-red-600">
             <XCircleIcon className="ml-5 h-8 w-8" />
           </button>
         </div>
